a1025e842cb5e03f6ced808f4368a0b6a2ade322
Kirill F <kirill.olegovich31@gmail.com>
2023-04-30 15:40:29 +0300

    async.gather() implemented to limit amount of api calls (untested)

--- server.py
+++ server.py
@@ -10,37 +10,44 @@ import events
 TOKEN = os.getenv("TELEGRAM_YOUTUBE_NOTIFIER_TOKEN")
 connection = Connection(TOKEN)
 
+async def incoming():
+    "This funcion handlins incoming messages"
+    while True:
+        update = await connection.get_updates()
+        # this part handles incoming messages
+        for update_id in sorted(update):
+            message, chat_id, username = update[update_id]
+            response = await message_handler(message, chat_id, username)
+
+            # check if you want preview on the message
+            if "/get_favorites" in message:
+                disable_preview = True
+            else:
+                disable_preview = False
+
+            await connection.send_message(chat_id, response, disable_web_page_preview=disable_preview)
+            await asyncio.sleep(1)
+
+async def send_updates():
+    "This function checks for new vidoes and sends updates to users"
+    while True:
+        #this part handles business logic and sends updates
+        videos_added = events.add_new_videos() 
+        for url, chat_id in videos_added:
+            await connection.send_message(text="NEW VIDEO!\n{}".format(url), chat_id=chat_id)
+
+        videos_added = []
+        await asyncio.sleep(900) 
+
+
 async def main():
     async with aiohttp.ClientSession() as session:
         connection.session = session
-
-        while True:
-            update = await connection.get_updates()
-
-            # this part handles incoming messages
-            for update_id in sorted(update):
-                message, chat_id, username = update[update_id]
-                response = await message_handler(message, chat_id, username)
-
-                # check if you want preview on the message
-                if "/get_favorites" in message:
-                    disable_preview = True
-                else:
-                    disable_preview = False
-
-                await connection.send_message(chat_id, response, disable_web_page_preview=disable_preview)
+        await asyncio.gather(incoming(), send_updates())
             
-            #this part handles business logic and sends updates
-            videos_added = events.add_new_videos() 
-            for url, chat_id in videos_added:
-                await connection.send_message(text="NEW VIDEO!\n{}".format(url), chat_id=chat_id)
-
-            videos_added = []
-            await asyncio.sleep(2) 
-
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
 
-
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
