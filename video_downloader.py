199b25636fb860a44a58d8292777ccab3c4612a5
cyrillus31 <kirill.olegovich31@gmail.com>
2023-05-24 17:23:23 +0300

    added logging

--- video_downloader.py
+++ video_downloader.py
@@ -1,6 +1,20 @@
 import pytube
 import os
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler("logs/pytube.log")
+
+formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(message)s",
+                              datefmt="%Y-%m-%d %H:%M:%S")
+
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
 
 def download_audio(url):
     try:
@@ -11,5 +25,6 @@ def download_audio(url):
         stream.download(output_path="downloads")
         return 1
     except Exception as e:
+        logger.exception(f"The following error happend while trying to download the audio:\n {e}")
         return 0
 
