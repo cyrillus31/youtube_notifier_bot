4cad60ed8f553084a294e83779bbe5ce8c3d40a2
cyrillus31 <kirill.olegovich31@gmail.com>
2023-09-09 21:55:18 +0300

    added english to /help message; channel_user.sql now orders users by date_added

--- db/scripts/database_access.sh
+++ db/scripts/database_access.sh
@@ -1,21 +1,36 @@
 #! /bin/sh
 
+echo -e "\nWhat sort of query do you want to run? See the following options:\n"
+
 select queryfile in *.sql
 do 
+	# run the query
 	cat $queryfile | sqlite3 ../data.db
-	echo query is done!
-	read -p "Want to proceede? y/n " answer
+	echo ""
+	echo The query is done!
+	echo ""
+	
+	# ask if the user wants to search again
+	read -p "Do you want to search again? y/n: " answer
 	echo ""
 
 	if [ $answer == "n" ]; then
-		echo -e "this is the end\r\n"
+		echo -e "The script is being terminated\r\n"
+		break 
+
+	elif [ $answer == "y" ]; then
+		true # pass
+	else
+		echo -e "I don't understand you. "
+		echo -e "Try again\r\n"
 		break
 	fi
-
+	
+	# print the list of available queries
 	i=0
 	for file in *.sql; do
 		i=$((i+1))
-		echo "$i)" $file 
+		echo "$i) $file"
 	done
 
 done
