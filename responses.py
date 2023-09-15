474c29cae6f70787ecff0a3a296a9ccfeb104ec6
cyrillus31 <kirill.olegovich31@gmail.com>
2023-09-15 15:09:07 +0300

    added english to /help message

--- responses.py
+++ responses.py
@@ -2,17 +2,43 @@ start = "Hi! User {} registered"
 
 added = "Channel was added!"
 
-help = """*Hi! This bot can send you updates from youtube channels you picked.*
+help = """*Hi! This bot can send you updates from youtube channels you picked. Updates may take from 15 to 40 minutes.*
 
-Команды:
-/start              - Зарегистрировать нового пользователя
-/help               - Прочитать это сообщение
-/add\_channel  url  - Добавь через пробел к этой команде url адрес ютуб-канала, чтобы получать обновления
-/latest             - Получи 10 последних видео c их _номерами_
-/add\_favorite      - Добавь к команде _номер_ видео, чтобы добавить это видео в избранное (номера последних 10 видео можно узнать в команде /latest)
-/get\_favorites     - Получи все видео, которые находятся в избранном 
+Commands:             
 
-По любым вопросам: @cyrillus31
+/start - to register a new user
+
+/help - to read this message
+
+/add\_channel `url` - type the command, after that type a space and then type the *url of the channel you want to add* to recieve updates, for example copy and paste this: 
+`/add_channel https://www.youtube.com/@Vsauce`
+
+/latest - to recieve last 10 videos with their _id_s
+
+/add\_favorite `id` - type the command, type a space and then type the _id_ of the video you want to save amongs your favorites 
+
+/get\_favorites - to recieve links to all the videos you saved in your favorites
+
+=================================
+
+Команды:             
+
+/start - зарегистрировать нового пользователя
+
+/help - прочитать это сообщение
+
+/add\_channel `url` - добавь через пробел к этой команде url адрес ютуб-канала, чтобы получать обновления, например скопируй и вставь:
+`/add_channel https://www.youtube.com/@Vsauce`
+
+/latest - получи 10 последних видео c их _номерами_
+
+/add\_favorite `id` - добавь к команде через пробел _номер_ видео, чтобы добавить это видео в избранное (номера последних 10 видео можно узнать в команде /latest)
+
+/get\_favorites - получи все видео, которые ты добавил в избранные 
+
+=================================
+
+For furhter information / По любым вопросам: @cyrillus31
 """
 
 audio_ok = 'Audio is being downloaded'
