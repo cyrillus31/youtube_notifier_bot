d2333d8876dc79a72932df11fefc18be082c665d
cyrillus31 <kirill.olegovich31@gmail.com>
2023-09-22 13:13:52 +0300

    updated admin bash script

--- db/scripts/videos_users.sql
+++ db/scripts/videos_users.sql
@@ -1,6 +1,10 @@
-select title, users.name from videos 
- join channel_user 
- on videos.channel_id=channel_user.channel_id 
-join users 
-on users.chat_id=channel_user.user_id 
-order by name;
+SELECT title, name FROM (
+  select title, users.name, users.date_added from videos 
+    join channel_user 
+      on videos.channel_id=channel_user.channel_id 
+    join users 
+      on users.chat_id=channel_user.user_id 
+    order by videos.date_added DESC
+  limit 50
+) as subquery
+order by date_added ASC;
